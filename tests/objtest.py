68e5025ede0f9f3131a18386a8447c7abb253801
Yuying Quan <yuyingquan@Yuyings-MacBook-Pro.local>
2021-03-26 13:48:10 -0700

    add license header in new test files

--- tests/objtest.py
+++ tests/objtest.py
@@ -1,3 +1,7 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/objprint/blob/master/NOTICE.txt
+
+
 class ObjTest:
     def __init__(self, attrs):
         for key, value in attrs.items():
