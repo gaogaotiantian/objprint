88b8d0ca6ec2015fa0628e4db165a6c20de1c8f5
Yuying Quan <yuyingquan@Yuyings-MacBook-Pro.local>
2021-03-29 22:43:40 -0700

    add tests for color and label

--- tests/test_objprint.py
+++ tests/test_objprint.py
@@ -80,3 +80,32 @@ class TestObjprint(ObjprintTestCase):
             output = buf.getvalue()
         expected = "<ObjTest\n .elem1 = 1,\n .elem2 = 2\n>\n"
         self.assertEqual(expected, output)
+
+    def test_color_without_label(self):
+        with io.StringIO() as buf, redirect_stdout(buf):
+            obj = ObjTest({"name": "Lisa"})
+            objprint(obj, color=True)
+            output = buf.getvalue()
+        seperate_string = output.splitlines()
+        seperate1 = seperate_string[0].split("<")
+        self.assertEqual("\x1b[36m", seperate1[0])
+        self.assertIn("\x1b[39m", seperate1[1])
+
+        seperate2 = seperate_string[1].split(".")
+        self.assertEqual("  \x1b[32m", seperate2[0])
+        self.assertIn("\x1b[39m", seperate2[1])
+
+        seperate3 = seperate_string[2].split(">")
+        self.assertEqual("\x1b[36m", seperate3[0])
+        self.assertEqual("\x1b[39m", seperate3[1])
+
+    def test_color_with_label(self):
+        with io.StringIO() as buf, redirect_stdout(buf):
+            obj = ObjTest({"name": "Lisa", "age": 19})
+            objprint(obj, color=True, label=["age"])
+            output = buf.getvalue()
+        seperate_string = output.splitlines()
+        seperate_label = seperate_string[1]
+        split_str = seperate_label.split(".")
+        self.assertEqual("  \x1b[33m", split_str[0])
+        self.assertIn("\x1b[39m", split_str[1])
