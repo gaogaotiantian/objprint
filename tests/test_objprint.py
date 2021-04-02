67a1d557b18616d5b70e2bf8cb77cf80eb2f684d
Yuying Quan <yuyingquan@Yuyings-MacBook-Pro.local>
2021-04-01 20:40:57 -0700

    change color tests more clear

--- tests/test_objprint.py
+++ tests/test_objprint.py
@@ -7,6 +7,7 @@ from contextlib import redirect_stdout
 
 from objprint import objprint
 from .objtest import ObjTest, ObjprintTestCase
+from objprint.color_util import COLOR
 
 
 class TestObjprint(ObjprintTestCase):
@@ -86,26 +87,30 @@ class TestObjprint(ObjprintTestCase):
             obj = ObjTest({"name": "Lisa"})
             objprint(obj, color=True)
             output = buf.getvalue()
-        seperate_string = output.splitlines()
-        seperate1 = seperate_string[0].split("<")
-        self.assertEqual("\x1b[36m", seperate1[0])
-        self.assertIn("\x1b[39m", seperate1[1])
-
-        seperate2 = seperate_string[1].split(".")
-        self.assertEqual("  \x1b[32m", seperate2[0])
-        self.assertIn("\x1b[39m", seperate2[1])
-
-        seperate3 = seperate_string[2].split(">")
-        self.assertEqual("\x1b[36m", seperate3[0])
-        self.assertEqual("\x1b[39m", seperate3[1])
+        self.assertIn(COLOR.CYAN, output)
+        self.assertIn(COLOR.GREEN, output)
+        self.assertIn(COLOR.DEFAULT, output)
 
     def test_color_with_label(self):
         with io.StringIO() as buf, redirect_stdout(buf):
             obj = ObjTest({"name": "Lisa", "age": 19})
             objprint(obj, color=True, label=["age"])
             output = buf.getvalue()
-        seperate_string = output.splitlines()
-        seperate_label = seperate_string[1]
-        split_str = seperate_label.split(".")
-        self.assertEqual("  \x1b[33m", split_str[0])
-        self.assertIn("\x1b[39m", split_str[1])
+        self.assertIn(COLOR.YELLOW, output)
+        self.assertIn(COLOR.DEFAULT, output)
+
+    def test_no_color(self):
+        with io.StringIO() as buf, redirect_stdout(buf):
+            obj = ObjTest({"Age": 10, "grade": 5})
+            objprint(obj, color=False)
+            output = buf.getvalue()
+        self.assertNotIn(COLOR.CYAN, output)
+        self.assertNotIn(COLOR.DEFAULT, output)
+
+    def test_label_only(self):
+        with io.StringIO() as buf, redirect_stdout(buf):
+            obj = ObjTest({"Age": 10, "grade": 5})
+            objprint(obj, color=False, label=['grade'])
+            output = buf.getvalue()
+        self.assertNotIn(COLOR.CYAN, output)
+        self.assertIn(COLOR.YELLOW, output)
