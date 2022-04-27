e41a3455e028b9a2d85a37ba157b85a54d2268b6
gaogaotiantian <gaogaotiantian@hotmail.com>
2022-04-27 11:45:34 -0700

    Fix color on windows terminal without VT (#64)
    * Fix color on windows terminal without VT

--- src/objprint/color_util.py
+++ src/objprint/color_util.py
@@ -1,6 +1,8 @@
 # Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
 # For details: https://github.com/gaogaotiantian/objprint/blob/master/NOTICE.txt
 
+import sys
+
 
 class COLOR:
     BLACK = '\033[30m'
@@ -14,5 +16,21 @@ class COLOR:
     DEFAULT = '\033[39m'
 
 
+color_support = True
+
+
+if sys.platform == "win32":
+    try:
+        # https://stackoverflow.com/questions/36760127/...
+        # how-to-use-the-new-support-for-ansi-escape-sequences-in-the-windows-10-console
+        from ctypes import windll
+        kernel32 = windll.kernel32
+        kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+    except Exception:  # pragma: no cover
+        color_support = False
+
+
 def set_color(s, color):
-    return f"{color}{s}{COLOR.DEFAULT}"
+    if color_support:
+        return f"{color}{s}{COLOR.DEFAULT}"
+    return f"{s}"  # pragma: no cover
